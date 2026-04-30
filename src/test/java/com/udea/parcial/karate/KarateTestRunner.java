005910e20969fba96040fedfc054b7fbd69804ec
JuanJoM14 <juan.monsalveh1@udea.edu.co>
2026-04-30 13:17:47 -0500

    Remove old controllerTest

--- src/test/java/com/udea/parcial/karate/KarateTestRunner.java
+++ src/test/java/com/udea/parcial/karate/KarateTestRunner.java
@@ -1,21 +1,70 @@
 package com.udea.parcial.karate;
 
 import com.intuit.karate.junit5.Karate;
-import com.udea.parcial.ParcialApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Runner para ejecutar todas las pruebas de integración de Karate
- * Ejecuta todos los archivos .feature en el classpath
- */
-@SpringBootTest(
-    classes = ParcialApplication.class,
-    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
-)
+
 public class KarateTestRunner {
 
     @Karate.Test
     Karate testAll() {
         return Karate.run().relativeTo(getClass());
     }
+
+    @Karate.Test
+    Karate testConsultarInventarioExistente() {
+        return Karate.run()
+                .scenarioName("Consultar inventario de un almacén existente")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testConsultarSinHeader() {
+        return Karate.run()
+                .scenarioName("Consultar inventario sin header de versión")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testConsultarVersionIncorrecta() {
+        return Karate.run()
+                .scenarioName("Consultar inventario con versión incorrecta")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testConsultarAlmacenVacio() {
+        return Karate.run()
+                .scenarioName("Consultar inventario de almacén sin productos")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testRegistrarNuevoProducto() {
+        return Karate.run()
+                .scenarioName("Registrar nuevo producto en inventario")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testRegistrarAlmacenInexistente() {
+        return Karate.run()
+                .scenarioName("Registrar producto con almacén inexistente")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testRegistrarSinVersion() {
+        return Karate.run()
+                .scenarioName("Registrar producto sin versión de API")
+                .relativeTo(getClass());
+    }
+
+
+    @Karate.Test
+    Karate testCrearYVerificar() {
+        return Karate.run()
+                .scenarioName("Crear producto y verificar que aparece en el inventario del almacén")
+                .relativeTo(getClass());
+    }
+
 }
